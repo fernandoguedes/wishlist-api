0156339e6b18cc29684585b7120f454eecb0453b
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2020-09-23 19:08:14 -0300

    Add before handler to check auth

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
-const customersController = require('../controllers/customer.controller')
-const wishlistController = require('../controllers/wishlist.controller')
-const customersSchema = require('../schemas/customers.schema')
-const wishlistSchema = require('../schemas/wishlists.schema')
+const customersController = require('./controllers/customer.controller')
+const wishlistController = require('./controllers/wishlist.controller')
+const authController = require('./controllers/auth.controller')
+const customersSchema = require('./schemas/customers.schema')
+const wishlistSchema = require('./schemas/wishlists.schema')
+const authSchema = require('./schemas/auth.schema')
 
 const routes = [
   {
@@ -12,32 +14,43 @@ const routes = [
   {
     method: 'POST',
     url: '/customers',
+    beforeHandler: authController.validateToken,
     handler: customersController.add,
     schema: customersSchema.add
   },
   {
     method: 'GET',
     url: '/customers/:id',
+    beforeHandler: authController.validateToken,
     handler: customersController.get,
     schema: customersSchema.get
   },
   {
     method: 'PUT',
     url: '/customers/:id',
+    beforeHandler: authController.validateToken,
     handler: customersController.put,
     schema: customersSchema.put
   },
   {
     method: 'DELETE',
     url: '/customers/:id',
+    beforeHandler: authController.validateToken,
     handler: customersController.delete,
     schema: customersSchema.delete
   },
   {
     method: 'POST',
     url: '/wishlist',
+    beforeHandler: authController.validateToken,
     handler: wishlistController.add,
     schema: wishlistSchema.add
+  },
+  {
+    method: 'POST',
+    url: '/auth/login',
+    handler: authController.login,
+    schema: authSchema.login
   },
 ]
 
