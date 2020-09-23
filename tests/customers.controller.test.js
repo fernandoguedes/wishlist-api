6137d9c346a86c06b513846b288b4b1c1823833a
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2020-09-22 22:11:11 -0300

    Add spec of GET customers endpoint

--- tests/customers.controller.test.js
+++ tests/customers.controller.test.js
@@ -77,4 +77,45 @@ describe('Customers', () => {
       done()
     })
   })
+
+  describe('Get customer', () => {
+    test('responds with success on request GET /customers when customer exists', async (done) => {
+      const savedCustomer = await app.inject({
+        method: 'POST',
+        url: '/customers',
+        body: {
+          email: 'customer@wishlist.com.br',
+          name: 'Wishlist et al'
+        }
+      })
+
+      const payload = JSON.parse(savedCustomer.payload)
+
+      const response = await app.inject({
+        method: 'GET',
+        url: `/customers/${payload.id}`
+      })
+
+      const body = JSON.parse(response.body)
+  
+      expect(response.statusCode).toBe(200)
+      expect(body).toHaveProperty('id')
+      expect(body).toHaveProperty('name')
+      expect(body).toHaveProperty('email')
+      done()
+    })
+
+    test('responds with 404 on request GET /customers when customer id not exists', async (done) => {
+      const response = await app.inject({
+        method: 'GET',
+        url: `/customers/5f6a9e29fc90181c23e37c68`
+      })
+
+      const body = JSON.parse(response.body)
+  
+      expect(response.statusCode).toBe(404)
+      expect(body).toHaveProperty('error')
+      done()
+    })
+  })
 })
