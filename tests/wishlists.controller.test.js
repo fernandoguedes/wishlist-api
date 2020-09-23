75a8328bbdb40b7410b0c52dbb9af8b6e08e82b1
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2020-09-23 15:13:59 -0300

    Add more specs to wishlist

--- tests/wishlists.controller.test.js
+++ tests/wishlists.controller.test.js
@@ -54,6 +54,54 @@ describe('Wishlists', () => {
       done()
     })
 
+    test('responds with success on request POST /wishlist when add two different products', async (done) => {
+      const customer = await app.inject({
+        method: 'POST',
+        url: '/customers',
+        body: {
+          email: 'different@wishlist.com',
+          name: 'Wishlist et al'
+        }
+      })
+
+      const body = JSON.parse(customer.payload)
+
+      const productWishlist1 = await app.inject({
+        method: 'POST',
+        url: '/wishlist',
+        body: {
+          product_id: '77be5ad3-fa87-d8a0-9433-5dbcc3152fac',
+          customer_id: body.id,
+        }
+      })
+
+      const productWishlist2 = await app.inject({
+        method: 'POST',
+        url: '/wishlist',
+        body: {
+          product_id: '1bf0f365-fbdd-4e21-9786-da459d78dd1f',
+          customer_id: body.id,
+        }
+      })
+
+      const parsedWishlist1 = JSON.parse(productWishlist1.payload)
+      const parsedWishlist2 = JSON.parse(productWishlist2.payload)
+
+      expect(productWishlist1.statusCode).toBe(201)
+      expect(parsedWishlist1).toHaveProperty('image')
+      expect(parsedWishlist1).toHaveProperty('price')
+      expect(parsedWishlist1).toHaveProperty('title')
+      expect(parsedWishlist1).toHaveProperty('url')
+
+      expect(productWishlist2.statusCode).toBe(201)
+      expect(parsedWishlist2).toHaveProperty('image')
+      expect(parsedWishlist2).toHaveProperty('price')
+      expect(parsedWishlist2).toHaveProperty('title')
+      expect(parsedWishlist2).toHaveProperty('url')
+
+      done()
+    })
+
     test('responds with error on request POST /wishlist when product does not exist', async (done) => {
       const customer = await app.inject({
         method: 'POST',
