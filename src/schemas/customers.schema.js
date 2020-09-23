7918eb394e0fe1d2d7b837b998d766e96b1ebd01
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2020-09-22 22:11:38 -0300

    Add endpoint and schema to get customer

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -1,4 +1,4 @@
-exports.addCustomerSchema = {
+exports.add = {
   description: 'Create a new customer',
   tags: ['Customer'],
   summary: 'Creates new customer with given values',
@@ -8,7 +8,8 @@ exports.addCustomerSchema = {
     properties: {
       email: { type: 'string' },
       name: { type: 'string' }
-    }
+    },
+    additionalProperties: false
   },
   response: {
     201: {
@@ -16,7 +17,32 @@ exports.addCustomerSchema = {
       type: 'object',
       properties: {
         id: { type: 'string' }
-      }
+      },
+      additionalProperties: false
+    }
+  }
+}
+
+exports.get = {
+  description: 'Get a customer',
+  tags: ['Customer'],
+  summary: 'Get customer by id',
+  params: {
+    id: {
+      type: 'string',
+      required: ['id']
+    } 
+  },
+  response: {
+    200: {
+      description: 'Successful response',
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        name: { type: 'string' },
+        email: { type: 'string' },
+      },
+      additionalProperties: false
     }
   }
 }
